bd3d5e0b1b940c76a9d870466c4cd23b0628a77d
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-17 12:04:31 +0800

    check-hardcoded-camera-sensors.sh: Enclose $STRINGS with quotes

--- check-hardcoded-camera-sensors.sh
+++ check-hardcoded-camera-sensors.sh
@@ -13,27 +13,27 @@ STRINGS="$(strings $BLOB)"
 if false; then
 # Actuator
 for ActuatorName in `grep -oP "(?<=<ActuatorName>)[^<]+" $XML`; do
-    echo $STRINGS | grep "$ActuatorName" > /dev/null || echo "Actuator $ActuatorName does not exist."
+    echo "$STRINGS" | grep "$ActuatorName" > /dev/null || echo "Actuator $ActuatorName does not exist."
 done
 # Chromatix
 for ChromatixName in `grep -oP "(?<=<ChromatixName>)[^<]+" $XML`; do
-    echo $STRINGS | grep "$ChromatixName" > /dev/null || echo "Chromatix $ChromatixName does not exist."
+    echo "$STRINGS" | grep "$ChromatixName" > /dev/null || echo "Chromatix $ChromatixName does not exist."
 done
 # Chromatix India
 if grep ChromatixName_india $XML > /dev/null; then
     for ChromatixName_india in `grep -oP "(?<=<ChromatixName_india>)[^<]+" $XML`; do
-        echo $STRINGS | grep "$ChromatixName_india" > /dev/null || echo "Chromatix India $ChromatixName_india does not exist."
+        echo "$STRINGS" | grep "$ChromatixName_india" > /dev/null || echo "Chromatix India $ChromatixName_india does not exist."
     done
 fi
 fi
 
 # Eeprom
 for EepromName in `grep -oP "(?<=<EepromName>)[^<]+" $XML`; do
-    echo $STRINGS | grep "$EepromName" > /dev/null || echo "Eeprom $EepromName does not exist."
+    echo "$STRINGS" | grep "$EepromName" > /dev/null || echo "Eeprom $EepromName does not exist."
 done
 # Sensor
 for SensorName in `grep -oP "(?<=<SensorName>)[^<]+" $XML`; do
-    echo $STRINGS | grep "$SensorName" > /dev/null || echo "Sensor $SensorName does not exist."
+    echo "$STRINGS" | grep "$SensorName" > /dev/null || echo "Sensor $SensorName does not exist."
 done
 
 exit 0
